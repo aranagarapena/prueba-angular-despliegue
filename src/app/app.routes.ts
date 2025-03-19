b7ea9c9f78fc5657d555e8f085d1e873c18f57a2
Arana Birt <arana.github@gmail.com>
2025-03-19 13:10:52 +0100

    Activando Hash Routing en Standalone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes, provideRouter, withHashLocation  } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Tus rutas aquí, por ejemplo:
+  { path: '', component: AppComponent }
+];
+
+export const appRouting = provideRouter(routes, withHashLocation());
