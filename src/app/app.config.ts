c76fe012ace86b5cdf2ed1fd47a3bc8bb6c9104f
Arana Birt <arana.github@gmail.com>
2025-03-19 13:29:09 +0100

    Corrigiendo doble carga de componente

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
-
 import { routes } from './app.routes';
+import { AppComponent } from './app.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation()) // 🔥 Activa Hash Routing
   ]
 };
