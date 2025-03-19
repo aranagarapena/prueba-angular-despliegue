e163bc1887326f294b381216625050ef01fc914b
Arana Birt <arana.github@gmail.com>
2025-03-19 13:12:25 +0100

    Corrigiendo Hash Routing en Standalone y regenerando build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation  } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()) // 🔥 Activamos Hash Routing aquí
-  ]};
+    provideRouter(routes, withHashLocation()) // 🔥 Activa Hash Routing
+  ]
+};
