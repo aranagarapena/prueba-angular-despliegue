4af693c7f123cb95780d7deebef41f992d146fed
Arana Birt <arana.github@gmail.com>
2025-03-20 09:08:36 +0100

    proyecto anterior borrado. Creada nueva estructura de directorios y proyecto de angular

--- desarrollo/prueba-despliegue/README.md
+++ desarrollo/prueba-despliegue/README.md
@@ -1,4 +1,4 @@
-# PruebaDespliegueAngular
+# PruebaDespliegue
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.1.
 
--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, provideRouter, withHashLocation  } from '@angular/router';
-import { AppComponent } from './app.component';
-
-export const routes: Routes = [
-    // Tus rutas aquí, por ejemplo:
-  { path: '', component: AppComponent }
-];
-
-export const appRouting = provideRouter(routes, withHashLocation());
